e6029264de01e7a5ba88048f0ff97ef0aa593eef
Clemens Hoffmann <cannelony@googlemail.com>
2011-07-06 15:59:37 +0200

    added testcase for browserify

---
+++ templates/vcard_connect.js
@@ -0,0 +1,12 @@
+var connect = require('connect');
+
+var server = connect.createServer();
+
+server.use(connect.static(__dirname));
+server.use(require('browserify')({
+    mount : '/browserify.js',
+    require : [ 'fs' ]
+}));
+
+server.listen(8001);
+console.log('Listening on 8001...');
